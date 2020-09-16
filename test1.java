e586be4e472c6f0341cb23e0fca0b3cd60947031
Justin Butler <jmichaelb@mun.ca>
2020-09-16 10:15:23 -0230

    Initial sharing of project

---
+++ test1.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class test1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test1
+{
+    // instance variables - replace the example below with your own
+    private test2 x;
+
+    /**
+     * Constructor for objects of class test1
+     */
+    public test1()
+    {
+        // initialise instance variables
+        x = new test2();
+    }
+
+
+}
